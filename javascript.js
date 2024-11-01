df5c04c088e8e42262c1cb0ab5c32bb816ef3c4e
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-11-01 13:33:41 -0300

    Add function to clear grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clear-btn");
 let squareTotal = 16*16;
 
 function createGrid() {
@@ -6,6 +7,26 @@ function createGrid() {
         const square = document.createElement("div");
         square.className = "square";
         container.appendChild(square);
+
+    fillGrid(square);
 }}
+
+function fillGrid(e) {
+    e.addEventListener("click", () => {
+        e.style.backgroundColor = "black";
+        e.style.border = "0";
+    })
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.removeAttribute("style");
+    })
+}
+
+clearBtn.addEventListener("click", () => {
+    clearGrid();
+})
 
 createGrid();
