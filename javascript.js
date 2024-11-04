bc73c7a6d5e530ed09030a1b7bb6cc6396648989
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-11-04 14:57:00 -0300

    Add toggle Grid button and Grid sizes

--- javascript.js
+++ javascript.js
@@ -1,27 +1,61 @@
 const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clear-btn");
-let squareTotal = 16*16;
+const toggleBtn = document.querySelector("#toggle-btn");
+const selectedSize = document.querySelector("input[type=range]");
+
+const gridSize = [16, 64, 100];
+const containerSize = 700;
 
 function createGrid() {
-    for (let i = 0; i < squareTotal; i++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
+    const gridSizePixel = gridSize[selectedSize.value - 1];
+    container.innerHTML = "";
+   
+    const squareSize = containerSize / gridSizePixel;
+
+    const totalSquares = gridSizePixel * gridSizePixel;
+    container.style.width = `${gridSizePixel * squareSize}px`;
+    container.style.height = `${gridSizePixel * squareSize}px`;
+
+    for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement("div");
+    square.className = "pixel";
+    square.className = "pixel-border";
+    square.className = "grid-border";
+    
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
+    container.appendChild(square);
 
     fillGrid(square);
-}}
+    clearGrid(square);
+    toggleGrid(square);
+  }
+}
 
 function fillGrid(square) {
-    square.addEventListener("click", () => {
-        square.style.backgroundColor = "black";
-        square.style.border = "0";
+  square.addEventListener("click", () => {
+    square.classList.remove("grid-border");
+    square.classList.add("pixel-border");
+    square.classList.add("pixel");
+  });
+}
+
+
+function clearGrid (square) {
+    clearBtn.addEventListener("click", () => {
+        square.classList.remove("pixel");
+        square.classList.remove("pixel-border");
+        square.classList.add("grid-border");
+})}
+
+function toggleGrid(square) {
+    toggleBtn.addEventListener("click", () => {
+        square.classList.toggle("grid-border");
     })
 }
 
-function clearGrid() {
-    document.querySelectorAll(".square").forEach(square => square.style = "");
-}
-
-clearBtn.addEventListener("click", clearGrid);
+selectedSize.addEventListener('input', createGrid);
 
 createGrid();
